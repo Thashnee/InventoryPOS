72c3ad987b6e7e6164e48e8c7897eb47c5291519
timo-n23-admin <thashnaidoo02@gmail.com>
2026-01-07 08:40:00 +0200

    Updated the db model

--- backend/models.py
+++ backend/models.py
@@ -1,7 +1,22 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
 from datetime import datetime
 
 db = SQLAlchemy()
+
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'username': self.username
+        }
 
 class Product(db.Model):
     __tablename__ = 'products'
